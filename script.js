e43e4e77b41305510e19ef43d1eba1e4f305b5e8
Daniel Barda <84409940+daniel-barda@users.noreply.github.com>
2023-10-20 16:41:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+"use strict";
+/*
+  Import the SVG icon file
+  This tells Vite that we need this file in the final build
+  So Vite will copy it to the `images` folder during build
+
+  Without the import, Vite doesn't know that we need the file
+  Even if we reference it in code like:
+    document.querySelector('img').src = '/images/icon.svg'
+  
+  We still need to import it so Vite sees that we depend on this file
+  Always import assets you want copied to build output folder
+*/
+
+import "./images/icon-arrow-up.svg";
+
+const navListEl = document.querySelector(".navigation__list");
+const listFeatureEL = document.querySelector(".list-feature");
+const listCompanyEl = document.querySelector(".list-company");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+
+/**
+ * Set the style of the overlay.
+ *
+ * @param {number} opacity - The opacity of the overlay, in the range from 0 to 1.
+ * @param {string} pointerEvents - The behavior of the overlay for click and drag events, such as "all" or "none."
+ * @param {string} visibility - The visibility of the overlay, such as "visible" or "hidden."
+ */
+
+function overlayStyle(opacity, pointerEvents, visibility) {
+  overlay.style.opacity = +opacity;
+  overlay.style.pointerEvents = pointerEvents;
+  overlay.style.visibility = visibility;
+}
+
+function toggleListImage(el) {
+  const active = el.classList.toggle("active");
+  const imgElement = el.closest("li").querySelector("img");
+
+  const isActive = active ? "arrow-up" : "arrow-down";
+  imgElement.alt = isActive;
+  imgElement.src = `images/icon-${isActive}.svg`;
+}
+
+header.addEventListener("click", (e) => {
+  if (e.target.closest(".btn-mobile-open")) {
+    header.classList.add("nav-open");
+    overlayStyle(1, "all", "visible");
+  }
+
+  if (e.target.closest(".btn-mobile-close")) {
+    header.classList.remove("nav-open");
+    overlayStyle(0, "none", "hidden");
+  }
+});
+
+navListEl.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const el = e.target.closest("li");
+
+  if (!el) return;
+
+  if (el.dataset.name === "Features") {
+    toggleListImage(listFeatureEL, "img");
+  }
+
+  if (el.dataset.name === "Company") {
+    toggleListImage(listCompanyEl, "img");
+  }
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 900) {
+    overlayStyle(0, "none", "hidden");
+  }
+
+  if (window.innerWidth <= 900 && header.classList.contains("nav-open")) {
+    overlayStyle(1, "all", "visible");
+  }
+});
