8dbb5c6ef64a8d8fd139059a576b3df893baa51c
Daniel Barda <84409940+daniel-barda@users.noreply.github.com>
2023-10-20 16:26:27 +0300

    prevent vite from changing the name of the files.
    prevent vite from changing the name of the files \ structure.

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,12 @@ export default {
   build: {
     rollupOptions: {
       output: {
+        entryFileNames: "script.js", // add this
         assetFileNames: "images/[name].[ext]",
       },
     },
+
+    // add this:
+    chunks: "index.js",
   },
 };
